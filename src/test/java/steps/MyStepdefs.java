fc717f4ed29ab1e992a0efa864729ab37b19ed8e
stannyweb <stannyweb@gmail.com>
2025-03-09 20:40:34 +0200

    feat: Refactor login test with Scenario Outline and improved step definitions
    This commit refactors the login test to use Scenario Outline for data-driven testing and improves step definitions for better readability and maintainability.
    
    Changes:
    - Updated Login.feature to use Scenario Outline with examples for username and password.
    - Modified step definition method to accept username and password as parameters.

--- src/test/java/steps/MyStepdefs.java
+++ src/test/java/steps/MyStepdefs.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,18 +24,20 @@ public class MyStepdefs {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
 
-    @When("I enter valid username and password")
-    public void i_enter_valid_username_and_password() {
+    @When("I enter valid {string} and {string}")
+    public void i_enter_valid_username_and_password(String username, String password) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username"))).sendKeys("admin");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username"))).sendKeys(username);
 
-        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.className("orangehrm-login-button")).click();
     }
 
     @Then("I should see the home page")
     public void i_should_see_the_home_page() {
+
+        Assert.assertEquals("OrangeHRM", driver.getTitle());
         System.out.println(driver.getTitle());
     }
 
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -4,13 +4,12 @@ Feature: Login functionality feature
   I want to login successfully
 
 
-  Scenario: Login Successful
+  Scenario Outline: Login Successful
     Given I am in the login page
-    When I enter valid username and password
+    When I enter valid <username> and <password>
     Then I should see the home page
-
-
-  Scenario: Login Successful
-    Given I am in the login
-    When I enter valid username and password
-    Then I should see the home page
+    Examples:
+      | username  | password        |
+      | "admin"   | "admin123"      |
+      | "Stan"    | "password123"   |
+      | "Angelov" | "wrongPassword" |
